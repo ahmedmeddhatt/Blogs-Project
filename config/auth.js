a438908c79898cdd66d7356ebc4c3254225e0d61
Ahmed Medhat <ahmedmedhat1231@gmail.com>
2021-11-16 00:36:39 +0200

    .

--- config/auth.js
+++ config/auth.js
@@ -1,7 +1,6 @@
 const { StatusCodes } = require('http-status-codes');
 const jwt=require('jsonwebtoken')
-// const rbac=require("../rbac/rbac.js")
-
+const rbac=require('../rbac/rbac.js')
     module.exports=(endd)=>{
         return async(req,res,next)=>{
         let bearerToken=req.headers.authorization
@@ -10,7 +9,7 @@ const jwt=require('jsonwebtoken')
         var decoded = jwt.verify(token, 'myToken');
         // console.log(decoded,"decoded")
         // console.log(token,"token");;
-        // const isAllowed=await rbac.can(decoded.role,endd)
+        const isAllowed=await rbac.can(decoded.role,endd)
         // console.log(isAllowed,"is");
         req.user=decoded
         // console.log(req.user,"gh");
