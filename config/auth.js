8ffb4dc073cb82778578f9baf77ca41caebb0d79
Ahmed Medhat <ahmedmedhat1231@gmail.com>
2021-11-16 00:32:35 +0200

    .

--- config/auth.js
+++ config/auth.js
@@ -1,6 +1,6 @@
 const { StatusCodes } = require('http-status-codes');
 const jwt=require('jsonwebtoken')
-const rbac=require("../rbac/rbac.js")
+// const rbac=require("../rbac/rbac.js")
 
     module.exports=(endd)=>{
         return async(req,res,next)=>{
@@ -8,16 +8,16 @@ const rbac=require("../rbac/rbac.js")
         let token
         token=bearerToken.split(" ")[1]
         var decoded = jwt.verify(token, 'myToken');
-        console.log(decoded,"decoded")
-        console.log(token,"token");;
-        const isAllowed=await rbac.can(decoded.role,endd)
-        console.log(isAllowed,"is");
+        // console.log(decoded,"decoded")
+        // console.log(token,"token");;
+        // const isAllowed=await rbac.can(decoded.role,endd)
+        // console.log(isAllowed,"is");
         req.user=decoded
-        console.log(req.user,"gh");
-        if(isAllowed){
+        // console.log(req.user,"gh");
+        // if(isAllowed){
             next()
-        }else{
-            res.status(StatusCodes.UNAUTHORIZED).json({message:"UNAUTHORIZED"})
-        }
+        // }else{
+            // res.status(StatusCodes.UNAUTHORIZED).json({message:"UNAUTHORIZED"})
+        // }
         }
     }
